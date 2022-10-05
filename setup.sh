76915b82cb4c4112d483011ad93abb8040aa6af1
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2022-10-04 22:14:09 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Place absolute path to base folder here!
+basefolder="/mnt/c/Users/lwalt/Desktop/External/CS2261/Grading/HW02_Submissions/"
+
+./bulkunzipper.sh $basefolder
+./taskreplacer.sh $basefolder
