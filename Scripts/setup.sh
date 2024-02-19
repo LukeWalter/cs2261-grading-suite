59b97c914f5331ade678488d38372773a923be87
Luke Walter <lwalter5667@gmail.com>
2024-02-18 23:31:53 -0500

    Updated comments/README

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -3,7 +3,7 @@
 # Relative path to base folder
 basefolder="../Example_Submissions/"
 
-# IMPORTANT: Make sure to switch out the tasks.json file here with your own!
+# NOTE: Make sure to switch out the tasks.json file here with your own!
 # Relative path to tasks.json
 mytasks="./tasks.json"
 
@@ -13,7 +13,7 @@ mytasks="./tasks.json"
 # To run: ./setup.sh
 
 # Troubleshooting scripts:
-# NOTE: Make sure you're in the Scripts folder when running these commands (And that you're using a WSL terminal!)
+# NOTE: Make sure you're in the Scripts folder when running any of these commands (And that you're using a WSL terminal!)
 
 # chmod 777 taskreplacer.sh bulkunzipper.sh setup.sh           | (Permissions denied)
 # sudo apt-get install zip unzip                               | (Unzip script not found)
